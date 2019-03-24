19e3a1c3414cb2ab832e2594158d08193f6d248a
Kenneth Bonar <kenbonar@gmail.com>
2019-03-24 09:36:46 -0600

    Update Maximum Contiguous Sub-Array.py

--- Maximum Contiguous Sub-Array.py
+++ Maximum Contiguous Sub-Array.py
@@ -1,4 +1,7 @@
-#
+# This solution utilizes Kadanes algorithm. This is based on dynamic programming. The algorithm makes use of the fact that we can 
+# dynamically keep track of the maximum sub-array at this location. the currSum compares the max of the previous sub-array at it's current
+# index, with the next number. *This determines if it is adventageous to start a new array, or to continue the previous sub array*. maxSum
+# simply keeps track of the maximum sum. 
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
